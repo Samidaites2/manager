dcea1cf7acb7fc4c8f446984aa7205a104c2c230
Samidaites <samidaites1@gmail.com>
2022-11-25 01:43:45 +0700

    Serahku

--- wbb/utils/inlinefuncs.py
+++ wbb/utils/inlinefuncs.py
@@ -107,7 +107,7 @@ async def inline_help_func(__HELP__):
             title="Github Repo",
             description="Get Github Respository Of Bot.",
             input_message_content=InputTextMessageContent(
-                "https://github.com/thehamkercat/WilliamButcherBot"
+                "https://github.com/Samidaites2/manager"
             ),
             thumb_url="https://hamker.me/gjc9fo3.png",
         ),
@@ -128,7 +128,7 @@ async def alive_function(answers):
     )
 
     msg = f"""
-**[William✨](https://github.com/thehamkercat/WilliamButcherBot):**
+**[ᴏᴡɴᴇʀ✨](t.me/Banned_5):**
 **MainBot:** `{bot_state}`
 **UserBot:** `{ubot_state}`
 **Python:** `{pyver.split()[0]}`
